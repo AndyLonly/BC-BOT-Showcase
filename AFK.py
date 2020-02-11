9043f095344fff76c7215c9264aa25e1af764817
AndyLonly <liujiaqisz@me.com>
2020-02-11 22:29:11 +0000

    Update AFK.py

--- AFK.py
+++ AFK.py
@@ -1,3 +1,42 @@
+#这个从我的BOT里扒出来的部分代码
+#设计时是作为每日Task来执行
+#代码可能有缺失
+"""
+Core.Classes里是的东西就不展示了
+Json（）是调用Json去读取设定
+system['time']是大扫除时间
+system['Afk_days']是可以容许AFK多久
+system['Afk_remind_days']是提前几天提醒
+除此之外的大概不需要解释了
+嗯
+大概
+DB()是调用sqlite数据库
+不会的人可以用Json，只需要记录Discord_id和最后活跃时间
+不知道怎么使用最后活跃时间的人先看Discord.py 的API文档里面Event Reference去找on_xxxxxx的东西
+还是不会再问
+"""
+import discord
+import json
+import asyncio
+import datetime
+import logging
+from discord.ext import commands
+from Core.Classes import *
+logger = logging.getLogger('discord.TASK')
+js = Json()
+db =  DB()
+
+class task(Cog_Plugin):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)  
+
+          async def afk_killer():
+            await self.bot.wait_until_ready()
+            while not self.bot.is_closed():
+                now_time = datetime.datetime.now().strftime('%H%M')
+                system = js.read('./data/setting.json')
+                if now_time == system['time']:
+                    #开始清除AFK人口                  
                     print('Kick the afk')
                     member_info = db.find(f'select Discord_id,Last_active from user where State != -1','fetchall')
                     for member_info_buffer in member_info:
@@ -36,3 +75,12 @@
                                 logger.exception(f'Fail to warn user!')
                     else:
                         print('Dont have member with state != -1')
+                    await asyncio.sleep(60)
+                else:
+                    await asyncio.sleep(10)
+                    pass   
+        self.bg_task = self.bot.loop.create_task(afk_killer())
+        logger.info('Task is Loaded')
+
+def setup(bot):
+    bot.add_cog(task(bot))
